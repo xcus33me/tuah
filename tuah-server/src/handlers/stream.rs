7b12a2d2d377f3d712884e19ebc1ae7ba9f9752a
xcus33me <HarvieOrigin@yandex.ru>
2025-03-05 16:16:39 +0300

    added routes

--- tuah-server/src/handlers/stream.rs
+++ tuah-server/src/handlers/stream.rs
@@ -1,13 +1,19 @@
-use axum::{extract::ws, response::IntoResponse, routing::get, Router};
+use std::sync::Arc;
 
-use crate::error::{ErrorResonse, HttpError};
+use axum::{extract::{ws, State}, response::IntoResponse, routing::get, Extension, Router};
 
-pub fn stream_handler -> Router {
+use crate::{error::{ErrorResponse, HttpError}, AppState};
+
+pub fn stream_handler() -> Router {
     Router::new()
         .route("/:ssuid", get(get_stream))
-        .route("/:ssuid/websocket", get(get_stream_ws));
+        .route("/:ssuid/websocket", get(get_stream_ws))
 }
 
-async fn get_stream() -> Result<impl IntoResponse, ErrorResonse> {}
+async fn get_stream(Extension(app_state): Extension<Arc<AppState>>) -> Result<impl IntoResponse, ErrorResponse> {
+    Ok("Stream".to_string())
+}
 
-async fn get_stream_ws() -> Result<impl IntoResponse, ErrorResonse> {}
+async fn get_stream_ws(Extension(app_state): Extension<Arc<AppState>>) -> Result<impl IntoResponse, ErrorResponse> {
+    Ok("Stream WebSocket".to_string())
+}
