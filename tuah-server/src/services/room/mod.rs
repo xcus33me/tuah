470514d0e169fd8e4984e23d295d74ff24348246
xcus33me <HarvieOrigin@yandex.ru>
2025-03-14 00:32:41 +0300

    feat: barebone RoomService

--- tuah-server/src/services/room/mod.rs
+++ tuah-server/src/services/room/mod.rs
@@ -1,3 +1,41 @@
-pub async fn create_or_get_room(uuid: String) {
-    
+use std::sync::Arc;
+
+use redis::RedisResult;
+use webrtc::turn::client;
+
+use crate::{handlers::room, types::room::Room};
+
+pub struct RoomService {
+    pub client: Arc<redis::Client>,
+}   
+
+impl RoomService {
+    pub fn from_client(client: redis::Client) -> Self {
+        Self {
+            client: Arc::new(client),
+        }
+    }
+
+    pub async fn create_or_get_room(uuid: String) -> RedisResult<Room> {
+        let mut con = self.client.get_async_connection().await?;
+        let key = format!("room:{}", uuid);
+
+        let room_json: Option<String> = con.get(&key).await?;
+
+        match room_json {
+            Some(json) => {
+
+            }
+            None => {
+                let room = Room::new();
+                let roomjson = serde_json::to_string(&room)?;
+                con.set(&key, room_json).await?;
+                Ok(room)
+            }
+        }
+    }
+
+    pub async fn delete_room(uuid: String) {
+
+    }
 }
